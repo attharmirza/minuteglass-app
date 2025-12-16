54bdc8ff4f9de3620395f284493ca825f877350d
Atthar Mirza <atthar.mirza@gmail.com>
2025-12-15 21:19:57 -0500

    Hourglass shape achieved (#1)
    * started timer logic
    
    * hourglass shape achieved!

--- src/view/Row.java
+++ src/view/Row.java
@@ -8,6 +8,21 @@ import java.awt.*;
  */
 public class Row extends JPanel {
     /**
+     * Setting the maximum size of a row, including hidden granules.
+     */
+    private int maxSize = 21;
+
+    /**
+     * The minimum size of a row is always 1 granule.
+     */
+    private int minSize = 1;
+
+    /**
+     * Initializing an array of empty granules to fill the row.
+     */
+    private Granule[] granules = new Granule[maxSize];
+
+    /**
      * Creates a default row of 20 empty granules if no granules are provided.
      */
     public Row() {
@@ -20,14 +35,50 @@ public class Row extends JPanel {
      * @param size The number of granules to add to the row.
      */
     public Row(int size) {
-        this.setLayout(new GridLayout(1, size, 5, 0));
+        this.setLayout(new GridLayout(1, maxSize, 5, 0));
         this.setBackground(Theme.BACKGROUND_COLOR);
         this.setOpaque(true);
 
-        for (int i = 0; i < size; i++) {
-            Granule granule = new Granule();
-            granule.setState(Granule.State.FILLED);
+        this.setSize(size);
+
+        for (Granule granule : granules) {
             this.add(granule);
+        }
+    }
+
+    /**
+     *
+     * @param size
+     * @throws IllegalArgumentException
+     */
+    private void setSize(int size) throws IllegalArgumentException {
+        if (size < minSize || size > maxSize) {
+            throw new IllegalArgumentException("Size must be between " + minSize + " and " + maxSize);
+        }
+
+        if (size % 2 != 1) {
+            throw new IllegalArgumentException("Size must be an odd number.");
+        }
+
+        for (int i = 0; i < granules.length; i++) {
+            int distanceFromCenter = Math.abs(i - (granules.length / 2));
+
+            Granule toBeAdded = new Granule();
+
+            if (distanceFromCenter == 0) {
+                toBeAdded.setState(Granule.State.FILLED);
+                granules[i] = toBeAdded;
+                continue;
+            }
+
+            if (distanceFromCenter <= size / 2) {
+                toBeAdded.setState(Granule.State.FILLED);
+                granules[i] = toBeAdded;
+                continue;
+            }
+
+            toBeAdded.setState(Granule.State.BLANK);
+            granules[i] = toBeAdded;
         }
     }
 }
