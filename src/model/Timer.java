f9403f1c1779d85bca404ffa3ff22b4ebd8e35f3
Atthar Mirza <atthar.mirza@gmail.com>
2025-12-16 16:39:40 -0500

    built timer model class

--- src/model/Timer.java
+++ src/model/Timer.java
@@ -6,24 +6,62 @@ package model;
  * remaining.
  */
 public class Timer {
-    private int totalSeconds;
-    private int remainingSeconds;
+    private int totalTime;
+    private int remainingTime;
 
     /**
      * No argument constructor, initializes the Timer to 5 minutes.
      */
     public Timer() {
-        this.totalSeconds = 60 * 5;
-        this.remainingSeconds = this.totalSeconds;
+        this.totalTime = 5 * 60 * 1000;
+        this.remainingTime = this.totalTime;
     }
 
     /**
-     * Constructor that initializes the Timer to a specific number of seconds.
-     * 
-     * @param seconds Initial timer length
+     * Constructor that initializes the Timer to a specific number of milliseconds.
+     *
+     * @param milliseconds Initial timer length
      */
-    public Timer(int seconds) {
-        this.totalSeconds = seconds;
-        this.remainingSeconds = this.totalSeconds;
+    public Timer(int milliseconds) {
+        this.totalTime = milliseconds;
+        this.remainingTime = this.totalTime;
+    }
+
+    /**
+     * Constructor that initializes the Timer to a specific number of seconds and
+     * milliseconds.
+     */
+    public Timer(int seconds, int milliseconds) {
+        this.totalTime = seconds * 1000 + milliseconds;
+        this.remainingTime = this.totalTime;
+    }
+
+    /**
+     * Constructor that initializes the Timer to a specific number of minutes and
+     * seconds.
+     */
+    public Timer(int minutes, int seconds, int milliseconds) {
+        this.totalTime = minutes * 60 * 1000 + seconds * 1000 + milliseconds;
+        this.remainingTime = this.totalTime;
+    }
+
+    /**
+     * Starts a timer loop that chops up the length of the timer into a total number
+     * of intervals, and then decrements the remaining time with a thread sleep.
+     *
+     * @param pieces The number of intervals to chop the timer into.
+     */
+    public void startTimer(int pieces) {
+        int interval = totalTime / pieces;
+
+        while (remainingTime > 0) {
+            remainingTime -= interval;
+
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
